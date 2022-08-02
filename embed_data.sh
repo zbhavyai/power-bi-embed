ea625661d083306330573f491d1eaf88408bc74c
Bhavyai Gupta <zbhavyai@gmail.com>
2022-08-01 21:13:45 -0600

    fetching required embed details using curl

---
+++ embed_data.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Author: Bhavyai Gupta
+
+
+# form data
+client_id=''
+grant_type='password'
+resource='https://analysis.windows.net/powerbi/api'
+username=''
+password=''
+scope='openid'
+group_id=''
+report_id=''
+
+
+# get the access token using email/password for subsequent requests
+ACCESS_TOKEN=$( curl -X POST --data-urlencode "client_id=${client_id}" --data-urlencode "grant_type=${grant_type}" --data-urlencode "resource=${resource}" --data-urlencode "username=${username}" --data-urlencode "password=${password}" --data-urlencode "scope=${scope}" https://login.windows.net/common/oauth2/token 2> /dev/null | jq -r '.access_token' )
+
+
+
+# get embed URL for the first (one and only in test setup) report
+EMBED_URL=$( curl -X GET -H "Authorization: Bearer ${ACCESS_TOKEN}" https://api.powerbi.com/v1.0/myorg/groups/${group_id}/reports 2> /dev/null | jq -r '.value [0] .embedUrl' )
+
+# escape & to prevent problems with sed (https://stackoverflow.com/a/28589149)
+EMBED_URL_MOD=$( echo ${EMBED_URL} | sed 's/&/\\&/g' )
+
+
+
+# get the embed token
+EMBED_TOKEN=$( curl -X POST -H "Authorization: Bearer ${ACCESS_TOKEN}" -d '{"accessLevel": "View", "allowSaveAs": "false"}' https://api.powerbi.com/v1.0/myorg/groups/${group_id}/reports/${report_id}/GenerateToken 2> /dev/null | jq -r '.token' )
+
+
+
+sed "s#EMBED_URL_HERE#${EMBED_URL_MOD}#g" ./src/Config_Template.tmp > ./src/Config.tmp
+sed "s#EMBED_TOKEN_HERE#${EMBED_TOKEN}#g" ./src/Config.tmp > ./src/Config.tsx
+rm ./src/Config.tmp
